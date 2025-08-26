2734041a2bb63f222a2048f7988235e6271e8b5c
cnezho4ek <75062808+cnezho4ek@users.noreply.github.com>
2025-08-26 18:27:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
-TOKEN = os.environ.get("TOKEN")  # будем использовать переменные окружения
+TOKEN = os.environ.get("TOKEN")  
+WEBHOOK_URL = os.environ.get("WEBHOOK_URL") 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Скоро отвечу прямо сюда или в лс. Если не отвечаю в течении 20-30 минут, попробуй другие способы связи со мной: https://neopryaten.online\n\nHey! I’ll reply here or in DMs soon. If I don’t respond within 20–30 minutes, try other ways to contact me: https://neopryaten.online")
@@ -10,4 +11,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
-app.run_polling()
+app.run_webhook(
+    listen="0.0.0.0",
+    port=int(os.environ.get("PORT", 10000)),
+    webhook_url=f"{WEBHOOK_URL}/{TOKEN}"
+)
