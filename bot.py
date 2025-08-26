c3407706b441e63dafd4b617c13884dd1cba0b69
cnezho4ek <75062808+cnezho4ek@users.noreply.github.com>
2025-08-26 17:36:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Messa
 TOKEN = os.environ.get("TOKEN")  # будем использовать переменные окружения
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Скоро отвечу прямо сюда или в лс. Если не отвечаю в течении 20-30 минут, попробуй дургие способы связи со мной: https://neopryaten.online \n\n Hey! I’ll reply here or in DMs soon. If I don’t respond within 20–30 minutes, try other ways to contact me: https://neopryaten.online")
+    await update.message.reply_text("Привет! Скоро отвечу прямо сюда или в лс. Если не отвечаю в течении 20-30 минут, попробуй дургие способы связи со мной: https://neopryaten.online\n\nHey! I’ll reply here or in DMs soon. If I don’t respond within 20–30 minutes, try other ways to contact me: https://neopryaten.online")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
